5be92582a1751d757bf459598ef886e69e885707
Alberto Nascimento <beto.bdn@gmail.com>
2021-07-18 20:21:26 -0300

    Add animation

--- app.js
+++ app.js
@@ -20,6 +20,18 @@ form.addEventListener('submit', event => {
         }        
     })
 
-    finalResult.querySelector('span').textContent = `${score}%`
+    scrollTo(0, 0)
+
     finalResult.classList.remove('d-none')
+
+    let counter = 0
+
+    const timer = setInterval(() => {
+        if(counter === score) {
+            clearInterval(timer)
+        }
+
+        finalResult.querySelector('span').textContent = `${counter}%`
+        counter ++
+    }, 10)
 })
