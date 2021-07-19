4559acaf4497cafe7dc720df9883f13c3eccebb2
Alberto Nascimento <beto.bdn@gmail.com>
2021-07-19 09:50:13 -0300

    Improving JS code

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const form = document.querySelector('.quiz-form')
 const finalResult = document.querySelector('.result')
 
-const correctAnswers = ['B', 'B', 'B', 'B']
+const correctAnswers = ['D', 'B', 'C', 'A']
 
-form.addEventListener('submit', event => {
-    event.preventDefault()
-        
-    let score = 0
+let score = 0
+
+const getUserAnswers = () => {
     const userAnswers = [
         form.inputQuestion1.value,
         form.inputQuestion2.value,
@@ -14,16 +13,23 @@ form.addEventListener('submit', event => {
         form.inputQuestion4.value,
     ]
 
+    return userAnswers
+}
+
+const calculateUserScore = userAnswers => {
     userAnswers.forEach((userAnswer, index) => {
         if(userAnswer === correctAnswers[index]) {
             score += 25
         }        
     })
+}
 
+const showFinalScore = () => {
     scrollTo(0, 0)
-
     finalResult.classList.remove('d-none')
+}
 
+const animateFinalScore = () => {
     let counter = 0
 
     const timer = setInterval(() => {
@@ -34,4 +40,15 @@ form.addEventListener('submit', event => {
         finalResult.querySelector('span').textContent = `${counter}%`
         counter ++
     }, 10)
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    const userAnswers = getUserAnswers()
+
+    calculateUserScore(userAnswers)
+
+    showFinalScore()
+
+    animateFinalScore()
 })
